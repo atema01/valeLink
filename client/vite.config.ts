2d23d13cf0c15c7111cbe585ce087761af2d9a0b
atema01 <atemafeydu17@gmail.com>
2026-02-05 21:49:53 +0300

    commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, __dirname, '');
     return {
       server: {
-        port: 3000,
+        port: 5173,
         host: '0.0.0.0',
+        strictPort: true,
       },
       plugins: [react()],
       define: {
